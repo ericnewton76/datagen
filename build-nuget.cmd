7890d1386e2ba3e8360455b3aaee201d5f64a24f
Eric Newton <eric.newton@lightmaker.com>
2015-12-02 13:13:01 -0500

    added build files

---
+++ build-nuget.cmd
@@ -0,0 +1,31 @@
+@ECHO OFF
+setlocal
+
+REM initialization
+set NUGET_EXE=nuget.exe 
+
+REM appveyor
+if not "%APPVEYOR_BUILD_VERSION%" == "" set BUILD_VERSION=%APPVEYOR_BUILD_VERSION%
+if not "%APPVEYOR_BUILD_VERSION%" == "" set NUGET_EXE=nuget
+
+REM checks
+echo %NUGET_EXE%
+if "%BUILD_VERSION%" == "" echo Missing BUILD_VERSION & goto :END
+
+if "%1" == "--after-build" goto :SKIP_BUILD
+
+echo.
+if "%1" == "--no-msbuild" goto :SKIP_BUILD
+call .\build.cmd & if errorlevel 1 goto :END
+shift
+
+:SKIP_BUILD
+
+REM Create Nuget Package
+echo %NUGET_EXE% pack datagen.nuspec -version %BUILD_VERSION%
+%NUGET_EXE% pack datagen.nuspec -version %BUILD_VERSION%
+
+REM if not "%1" == "--no-deploy" %NUGET_EXE% push
+shift
+
+:END
